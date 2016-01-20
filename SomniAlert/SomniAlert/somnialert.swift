45be81244a266f05b30b42744e82d269f7d2e6c5
Guled <somnibyte@gmail.com>
2016-01-19 22:11:56 -0600

    Added slant effect

--- SomniAlert/SomniAlert/somnialert.swift
+++ SomniAlert/SomniAlert/somnialert.swift
@@ -15,7 +15,6 @@ class SomniAlert:UIView {
      The mainView is the view that the SomniAlert sits on top of. The mainView, in this example, is the view used in the ViewController.swift file.
     */
     var mainView:UIView
-
     
     /**
      Default Initializer
@@ -24,9 +23,20 @@ class SomniAlert:UIView {
         
         self.mainView = mainView
         super.init(frame: frame)
+        
+        
+        // Configurations for the main SomniAlert view
         self.frame = CGRect(origin: self.mainView.center, size: CGSize(width: 200, height: 300))
-        self.center = self.mainView.center
-        self.backgroundColor = UIColor.whiteColor()
+        self.center = self.mainView.center // Place the view in the center of the users view
+        self.backgroundColor = UIColor.whiteColor() // Default background: white
+        self.clipsToBounds = true
+        
+        // Configurations for the slanted view
+        let slantedView = UIView(frame: CGRect(x: -10, y: -10, width: self.frame.size.width * 2, height: self.frame.size.height/2))
+        slantedView.backgroundColor = UIColor(red:0.204, green:0.231, blue:0.259, alpha:1.00)
+        slantedView.transform = CGAffineTransformMakeRotation(-50.0)
+        self.addSubview(slantedView)
+        
         
         
     }
